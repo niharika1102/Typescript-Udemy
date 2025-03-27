920efc19bb8610e624466002eebefe57a75be5c7
Niharika <niharikag1102@gmail.com>
2025-03-27 07:49:17 +0530

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { User } from "./User";
+import { Company } from "./Company";
+import "@types/google.maps"
+
+const user: User = new User();
+console.log(user);
+
+const company: Company = new Company();
+console.log(company);
