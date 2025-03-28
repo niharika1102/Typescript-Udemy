ba154c171b95bc0b38692640b8473f6cd4924949
Niharika <niharikag1102@gmail.com>
2025-03-28 23:29:58 +0530

    Delete src directory

--- src/index.js
+++
@@ -1,3 +0,0 @@
-import { User } from "./User";
-const user = new User();
-console.log(user);
--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { User } from "./User";
-import { Company } from "./Company";
-import "@types/google.maps"
-
-const user: User = new User();
-console.log(user);
-
-const company: Company = new Company();
-console.log(company);
